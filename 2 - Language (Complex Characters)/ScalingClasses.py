698cbb6266e30c7570ff2c85601bc03350270c80
cm600286 <91765679+cm600286@users.noreply.github.com>
2022-04-05 01:10:30 -0700

    Update ScalingClasses.py

--- 2 - Language (Complex Characters)/ScalingClasses.py
+++ 2 - Language (Complex Characters)/ScalingClasses.py
@@ -95,7 +95,7 @@ class HebrewScaling(ScalingProtocol):
     def showImage(self, set, showTarget, size):
         super().showImage(set, showTarget, size, 'Hebrew Characters')
 
-class ChineseScaling(ScalingProtocol):
+class NonsenseScaling(ScalingProtocol):
     trialsPerSet = 32
     winners = ['cm600286', 'Mila', 'KayLA', 'Minerva', 'Arisvt', 'RNFO', 'Bot6', 'Snoopy', 'Ana', 'BruinCub']
     highScores = [85696, 85646, 85191, 84935, 82726, 81222, 79835, 78097, 77787, 71178]
