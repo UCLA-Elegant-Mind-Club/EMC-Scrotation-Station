6975cb85fbc8c149c991747e02db5282be4e584d
cm600286 <91765679+cm600286@users.noreply.github.com>
2022-04-25 20:23:58 -0700

    separation of scaling/rotation combination data

--- Main.py
+++ Main.py
@@ -3,69 +3,74 @@ from psychopy.sound import Sound
 prefs.hardware['audioLib'] = ['ptb', 'pyo']
 import os, time
 from TVStimuli import TVStimuli as TV
-from ScrotationClasses import *
-from RotationClasses import *
 import csv
 
-longBreakTime = 60
-TV.debug = True
-if TV.debug:
-    debugDlg = gui.Dlg(title='Debug Mode?', pos=None, size=None, style=None,\
-         labelButtonOK=' Yes ', labelButtonCancel=' No ', screen=-1)
-    debugDlg.show()
-    TV.debug = debugDlg.OK
-    
-if TV.debug:
-        TV.trialsPerSet = 5
-        TV.initialPracticeTrials = 3
-        TV.trainingTime = 1
-        TV.interimPracticeTrials = 0
-        TV.prePracticeBreak = 1
-        TV.postPracticeBreak = 1
-        TV.postSetBreak = 1
-        TV.dummyTrials = 1
-        longBreakTime = 1
-
 def setup():
+    longBreakTime = 60
+    TV.debug = True
+    if TV.debug:
+        debugDlg = gui.Dlg(title='Debug Mode?', pos=None, size=None, style=None,\
+             labelButtonOK=' Yes ', labelButtonCancel=' No ', screen=-1)
+        debugDlg.show()
+        TV.debug = debugDlg.OK
+        
+    if TV.debug:
+            TV.trialsPerSet = 5
+            TV.initialPracticeTrials = 3
+            TV.trainingTime = 1
+            TV.interimPracticeTrials = 0
+            TV.prePracticeBreak = 1
+            TV.postPracticeBreak = 1
+            TV.postSetBreak = 1
+            TV.dummyTrials = 1
+            longBreakTime = 1
+
+def calibrate():
     calibDlg = gui.Dlg(title='Testing location?', pos=None, size=None, style=None,\
          labelButtonOK=' Local at Knudson ', labelButtonCancel=' Remotely ', screen=-1)
     calibDlg.show()
     standardCalibration = calibDlg.OK
 
+    #standard calibration uses Knudson TV data
+    #if taking data remotely, replace the calibration file with your own before running code
     if standardCalibration:
         TV.calibrate(os.path.join(os.getcwd(), 'Calibration', 'eccentricity_monitor_calibration_Knudson.csv'))
     else:
         TV.calibrate(os.path.join(os.getcwd(), 'Calibration', 'eccentricity_monitor_calibration.csv'))
 
-def getProtocol():
+def getProtocols():
     protocolFile = list(csv.reader(open('GroupProtocols.csv'), delimiter=','))
     
     # Select group project
     groupDialog = gui.Dlg(title='Select Group Project', screen=-1)
     groups = [row[0] for row in protocolFile]
     groupDialog.addField('Group: ', choices = groups[1:])
-    group = groupDialog.show()
-    if group is None:
+    groupInfo = groupDialog.show()
+    if groupInfo is None:
         core.quit()
     
     # Select protocol to run
     protocolDialog = gui.Dlg(title='Select protocol to run', screen=-1)
     protocols = protocolFile[groups.index(groupInfo[0])][1]
     protocolDialog.addField('Protocol: ', choices = protocols.split('. '))
-    protocol = protocolDialog.show()
-    if protocol is None:
+    protocolName = protocolDialog.show()
+    if protocolName is None:
         core.quit()
-    protocol = globals()[group[0] + protocol[0].replace(' ','')]
+    protocol = globals()['_' + protocolName[0].replace(' ','')]
     
     codeInfo = {'Participant Name': ''}
     codeDialog = gui.DlgFromDict(dictionary = codeInfo, sortKeys = False, title = 'Participant Info')
     if codeDialog.OK == False:
         core.quit()
     
+    dataFolder = os.path.join(os.getcwd(), str(groupInfo[0]), 'Data')
+    if not os.path.isdir(dataFolder):
+        os.mkdir(dataFolder)
+    dataFolder = os.path.join(dataFolder, protocolName[0])
+    if not os.path.isdir(dataFolder):
+        os.mkdir(dataFolder)
     
-    makeDataDirs(codeInfo['Participant Name'])
-    protocolList = getProtocolList(protocolInfo[0], codeInfo['Participant Name'],
-        os.path.join(os.getcwd(), 'Data'))
+    return [protocol(os.path.join(dataFolder, codeInfo[0]))]
 
 def makeDataDirs(group, protocol):
     dataFolder = os.path.join(os.getcwd(), str(group), 'Data')
@@ -73,10 +78,6 @@ def makeDataDirs(group, protocol):
         os.mkdir(dataFolder)
     for protName in protocolNames:
         os.mkdir(os.path.join(dataFolder, protName))
-
-protocolDict = {
-    
-
 
 def getProtocolList(group, participantCode, dirPath):
     participant = participantCode + '_' + time.strftime("%m_%d")
@@ -118,18 +119,20 @@ def protocolBreak():
             TV.showWait()
 
 
-    
-for protocolNum in range(0, len(protocolList)):    
-    loadSounds()
-    protocolList[protocolNum].main()
-    if protocolNum < len(protocolList) - 1: protocolBreak()
+def main():
+    for protocolNum in range(0, len(protocolList)):    
+        loadSounds()
+        protocolList[protocolNum].main()
+        if protocolNum < len(protocolList) - 1: protocolBreak()
 
-TV.playNotes(notes = [220, 277.18, 329.63, 277.18, 329.63, 440, 329.63, 440, 554.37, 440, 554.37, 659.25, 554.37, 659.25, 880],
-    beats = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4], beatLength = 0.1)
-TV.genDisplay('All done for today!', 0, 8, height = 3)
-TV.genDisplay('Thank you for your time!', 0, 4, height = 2)
-TV.genDisplay('Final Scores', 0, 0, height = 3)
-for i in range(0, len(protocolList)):
-    TV.genDisplay(str(protocolList[i].score), 0, -4 - 2 * i, color = [0,1,0])
-    print(protocolList[i].score)
-TV.showWait()
+    TV.playNotes(notes = [220, 277.18, 329.63, 277.18, 329.63, 440, 329.63, 440, 554.37, 440, 554.37, 659.25, 554.37, 659.25, 880],
+        beats = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4], beatLength = 0.1)
+    TV.genDisplay('All done for today!', 0, 8, height = 3)
+    TV.genDisplay('Thank you for your time!', 0, 4, height = 2)
+    TV.genDisplay('Final Scores', 0, 0, height = 3)
+    for i in range(0, len(protocolList)):
+        TV.genDisplay(str(protocolList[i].score), 0, -4 - 2 * i, color = [0,1,0])
+        print(protocolList[i].score)
+    TV.showWait()
+
+getProtocols()
