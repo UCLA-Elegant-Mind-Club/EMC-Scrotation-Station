99ade23e0065bcaac25ebb37e15628c3186c668d
alydrost <102986970+alydrost@users.noreply.github.com>
2022-05-03 15:31:39 -0700

    attention data

--- New Analysis Code/attentionGroup.m
+++ New Analysis Code/attentionGroup.m
@@ -7,8 +7,8 @@ end
 function [] = remakeFile(folder, fileName, correctCol, dirCol, eccCol, timeCol, sizeCol, targetCol)
     table = readtable(fullfile(folder, fileName));
     for row = 1:height(table)
-        size = str2num(table(row, sizeCol));
         data = table{row, :};
+        size = str2num(data(sizeCol));
         dir = data(dirCol);
         data = data([correctCol, eccCol, timeCol, targetCol]);
         data(2) = data(2) * (dir - 1);
