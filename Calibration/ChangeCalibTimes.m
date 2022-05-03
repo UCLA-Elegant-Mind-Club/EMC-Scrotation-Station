0e48c7c6636765798ea62710a6960d235307c90b
cm600286 <91765679+cm600286@users.noreply.github.com>
2022-05-03 01:20:53 -0700

    Update ChangeCalibTimes.m

--- Calibration/ChangeCalibTimes.m
+++ Calibration/ChangeCalibTimes.m
@@ -1,17 +1,20 @@
-monitorLabel = 'Mon 1';
-newTime = 91.54;
+monitorLabel = inputdlg(sprintf("Check 'monitors.csv' for label of system to change.\n\n" + ...
+    "Label for system:"), 'System Label', 1, {'Mon 1'});
 
 calibFile = 'monitors.csv';
 monTable = readtable(calibFile,'Delimiter','comma');
 monitorNum = find([monTable{:,1} + ""] == monitorLabel + "");
 oldTime = monTable{monitorNum, 21};
+newTime = inputdlg(sprintf("Old Calibration time was " + oldTime + " ms.\n\n" + ...
+    "New Calibration Time:"), 'Calibration Time', 1, [oldTime + ""]);
+newTime = str2num(newTime{1});
 monTable{monitorNum, 21} = newTime;
 
 dataFolder = rmDSStore(dir(fullfile(fileparts(pwd), 'Data')));
 for group = 3:length(dataFolder)
     groupFolder = rmDSStore(dir(fullfile(dataFolder(group).folder, dataFolder(group).name)));
     for protocol = 3:length(groupFolder)
-        protocolFolder = dir(fullfile(groupFolder(protocol).folder, groupFolder(protocol).name, "*[Mon " + monitorNum + "].csv"));
+        protocolFolder = dir(fullfile(groupFolder(protocol).folder, groupFolder(protocol).name, "*[" + monitorLabel + "].csv"));
         for file = 1:length(protocolFolder)
             try
                 fileName = fullfile(protocolFolder(file).folder, protocolFolder(file).name);
