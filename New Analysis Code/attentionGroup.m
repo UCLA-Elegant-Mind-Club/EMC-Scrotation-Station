1c310989f155fd63537a4d29a6592facb81dd78f
cm600286 <91765679+cm600286@users.noreply.github.com>
2022-05-03 16:22:44 -0700

    attention group

--- New Analysis Code/attentionGroup.m
+++ New Analysis Code/attentionGroup.m
@@ -1,18 +1,33 @@
 subFolder = uigetdir(fullfile(fileparts(pwd), 'Data'));
 fileList = dir(fullfile(subFolder, '*.csv'));
 for fileNum = 1:length(fileList)
-    remakeFile(subFolder, fileList(fileNum).name, 5, 2, 3, 4, 6, 1);
+    %remakeFile(subFolder, fileList(fileNum).name, 5, 2, 3, 4, 6, 1);
+    constantEcc(subFolder, fileList(fileNum).name, 5, 2, 3, 4, 6, 1);
 end
 
 function [] = remakeFile(folder, fileName, correctCol, dirCol, eccCol, timeCol, sizeCol, targetCol)
-    table = readtable(fullfile(folder, fileName));
-    for row = 1:height(table)
-        data = table{row, :};
-        size = str2num(data(sizeCol));
-        dir = data(dirCol);
-        data = data([correctCol, eccCol, timeCol, targetCol]);
-        data(2) = data(2) * (dir - 1);
+    tab = readtable(fullfile(folder, fileName));
+    for row = 1:height(tab)
+        data = table2cell(tab(row, :));
+        size = data{sizeCol};
+        dir = data{dirCol};
+        data = [data([correctCol, eccCol, timeCol]), 0];
+        data{2} = data{2} * (dir - 1);
+        data{3} = data{3} * 1000;
+        data = cell2table(data, 'VariableNames', {'Correct', 'Size', 'RT', 'Target'});
         mkdir(folder + "-size " + size);
-        writematrix(data, fullfile(folder + "-size " + size, fileName), 'WriteMode','append');
+        writetable(data, fullfile(folder + "-size " + size, fileName), 'WriteMode','append');
+    end
+end
+
+function [] = constantEcc(folder, fileName, correctCol, dirCol, eccCol, timeCol, sizeCol, targetCol)
+    mkdir(folder + " - reorganized");
+    tab = readtable(fullfile(folder, fileName));
+    for row = 1:height(tab)
+        data = table2cell(tab(row, :));
+        data = [data([correctCol, sizeCol, timeCol]), 0];
+        data{3} = data{3} * 1000;
+        data = cell2table(data, 'VariableNames', {'Correct', 'Size', 'RT', 'Target'});
+        writetable(data, fullfile(folder + " - reorganized", fileName), 'WriteMode','append');
     end
 end
